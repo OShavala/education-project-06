d91099b712c635ce654df1c0f8838c18890bbf0d
OShavala <olena.shvl@gmail.com>
2026-04-01 10:44:56 +0300

    fifth fixation

--- example-02.py
+++ example-02.py
@@ -1,1 +1,7 @@
+n = 3
+factorial = 1
+while n > 1:
+    factorial *= n
+    n -= 1
+print("Факторіал:", factorial)
 
---
+++ example-04.py
@@ -0,0 +1,7 @@
+i = 1
+while i <= 3:
+    print("Крок:", i)
+    i += 1
+else:
+    print("Завершено")
+
